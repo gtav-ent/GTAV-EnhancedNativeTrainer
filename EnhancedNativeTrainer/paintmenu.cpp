88de0275f6f47131dc8c6a5bd95462cf7e5592ac
TSM4278 <jksport2012@yahoo.com>
2015-05-19 01:33:34 -0500

    Pearlescent Working Now

--- EnhancedNativeTrainer/paintmenu.cpp
+++ EnhancedNativeTrainer/paintmenu.cpp
@@ -16,11 +16,11 @@ extern int whichpart = 0;
 extern int whichtype = 0;
 
 //Parts
-const std::vector<std::string> MENU_PAINT_WHAT{ "Primary Color", "Secondary Color" }; //"Pearl Topcoat", "Wheels"
+const std::vector<std::string> MENU_PAINT_WHAT{ "Primary Color", "Secondary Color", "Pearl Topcoat" }; //"Wheels"
 
 
 //Paint Names
-const std::vector<std::string> MENU_PAINT_TYPE{ "Normal", "Metallic", "Pearlescent", "Matte", "Metal", "Chrome" }; //"Wheel Colors"
+const std::vector<std::string> MENU_PAINT_TYPE{ "Metallic", "Normal", "Pearlescent", "Matte", "Metal", "Chrome" }; //"Wheel Colors"
 
 const std::vector<std::string> CAPTIONS_NORMAL{ "Black", "Carbon Black", "Graphite Black", "Anthracite Black", "Black Steel", "Dark Steel", "Silver", "Bluish Silver", "Rolled Steel", "Shadow Silver", "Stone Silver", "Midnight Silver", "Cast Iron Silver", "Red", "Torino Red", "Formula Red", "Lava Red", "Blaze Red", "Grace red", "Garnet Red", "Sunset Red", "Cabernet Red", "Wine Red", "Candy Red", "Hot Pink", "Pfister Pink", "Salmon Pink", "Sunrise Orange", "Orange", "Bright Orange", "Gold", "Bronze", "Yellow", "Race Yellow", "Dew Yellow", "Dark Green", "Racing Green", "Sea Green", "Olive Green", "Bright Green", "Gasoline Green", "Lime Green", "Midnight Blue", "Galaxy Blue", "Dark Blue", "Saxon Blue", "Blue", "Mariner Blue", "Harbor Blue", "Diamond Blue", "Surf Blue", "Nautical Blue", "Racing Blue", "Ultra Blue", "Light Blue", "Chocolate Brown", "Bison Brown", "Creek Brown", "Feltzer Brown", "Maple Brown", "Beechwood Brown", "Sienna Brown", "Saddle Brown", "Moss Brown", "Woodbeech Brown", "Straw Brown", "Sandy Brown", "Bleached Brown", "Schafter Purple", "Spinnaker Purple", "Midnight Purple", "Bright Purple", "Cream", "Ice White", "Frost White" };
 
@@ -34,7 +34,7 @@ const std::vector<std::string> CAPTIONS_METAL{ "Brushed Steel", "Brushed Black S
 
 const std::vector<std::string> CAPTIONS_CHROME{ "Chrome" };
 
-//const std::vector<std::string> CAPTIONS_WHEELS{ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40" };
+//const std::vector<std::string> CAPTIONS_WHEELS{ "Alloy", "Black", "Carbon Black", "Anthracite Black", "Black Steel", "Stone Silver", "Frost White", "Red", "Blaze Red", "Garnet Red", "Candy Red", "Sunset Red", "Salmon Pink", "Hot Pink", "Sunrise Orange", "Orange", "Bright Orange", "Gold", "Straw Brown", "Dark Copper", "Dark Ivory", "Dark Brown", "Bronze", "Dark Earth", "Desert Tan", "Yellow", "Race Yellow", "Yellow Bird", "Lime Green", "Pea Green", "Green", "Dark Green", "Olive Green", "Midnight Blue", "Royal Blue", "Baby Blue", "Bright Blue", "Flourescent Blue", "Slate Blue", "Schafter Purple", "Midnight Purple" };
 
 
 //Paint Values
@@ -53,9 +53,8 @@ const std::vector<std::string> VALUES_CHROME{ "0" };
 //const std::vector<std::string> VALUES_WHEELS{ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40" };
 
 
-const std::vector<std::string> VOV_PAINT_CAPTIONS[] = { CAPTIONS_NORMAL, CAPTIONS_METALLIC, CAPTIONS_PEARL, CAPTIONS_MATTE, CAPTIONS_METAL, CAPTIONS_CHROME }; //CAPTIONS_WHEELS
-
-const std::vector<std::string> VOV_PAINT_VALUES[] = { VALUES_NORMAL, VALUES_METALLIC, VALUES_PEARL, VALUES_MATTE, VALUES_METAL, VALUES_CHROME }; //VALUES_WHEELS
+const std::vector<std::string> VOV_PAINT_CAPTIONS[] = { CAPTIONS_METALLIC, CAPTIONS_NORMAL, CAPTIONS_PEARL, CAPTIONS_MATTE, CAPTIONS_METAL, CAPTIONS_CHROME }; //CAPTIONS_WHEELS
+const std::vector<std::string> VOV_PAINT_VALUES[] = { VALUES_METALLIC, VALUES_NORMAL, VALUES_PEARL, VALUES_MATTE, VALUES_METAL, VALUES_CHROME }; //VALUES_WHEELS
 
 
 
@@ -124,21 +123,34 @@ bool onconfirm_color_menu_selection(MenuItem<std::string> choice)
 		{
 			Vehicle veh = PED::GET_VEHICLE_PED_IS_USING(playerPed);
 			VEHICLE::SET_VEHICLE_MOD_KIT(veh, 0);
-			if (::whichpart == 0)
+
+			if (::whichpart == 0) //Primary Color
 			{
 				VEHICLE::SET_VEHICLE_MOD_COLOR_1(veh, ::whichtype, whichpaint, 0);
 			}
-			else if (::whichpart == 1)
+			else if (::whichpart == 1) //Secondary Color
 			{
 				VEHICLE::SET_VEHICLE_MOD_COLOR_2(veh, ::whichtype, whichpaint);
 			}
-			//else if (::whichpart == 2)
+			else if (::whichpart == 2) //Pearl Topcoat
+			{
+				Any paint1, paint2, paint3;
+				VEHICLE::GET_VEHICLE_MOD_COLOR_1(veh, &paint1, &paint2, &paint3);
+
+				if (paint1 == 2) //Check if pearlescent is set as primary paint type
+				{
+					VEHICLE::SET_VEHICLE_MOD_COLOR_1(veh, paint1, paint2, whichpaint); //apply topcoat
+				}
+				else
+				{
+					set_status_text("Set a Pearlescent Basecoat first");
+				}
+			}
+			//else if (::whichpart == 3) //Wheels
 			//{
-			//	VEHICLE::SET_VEHICLE_EXTRA_COLOURS(veh, whichpaint, 0); //pearl topcoat followed by wheel color? Need to play with this
-			//}
-			//else if (::whichpart == 3)
-			//{
-			//	VEHICLE::SET_VEHICLE_EXTRA_COLOURS(veh, 0, whichpaint); //pearl topcoat followed by wheel color? Need to play with this
+			//	Any paint1, paint2;
+			//	VEHICLE::GET_VEHICLE_EXTRA_COLOURS(veh, &paint1, &paint2);
+			//	VEHICLE::SET_VEHICLE_EXTRA_COLOURS(veh, paint1, whichpaint); //pearl topcoat followed by wheel color
 			//}
 
 		}
